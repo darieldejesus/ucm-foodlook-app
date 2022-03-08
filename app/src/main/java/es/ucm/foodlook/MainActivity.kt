d00d7b9afd8796964a84320ab6b2af42c00d8899
Ljmn30 <jmela@ucm.es>
2022-03-08 17:13:03 +0100

    Implementar boton para abrir camara

--- app/src/main/java/es/ucm/foodlook/MainActivity.kt
+++ app/src/main/java/es/ucm/foodlook/MainActivity.kt
@@ -1,6 +1,14 @@
 package es.ucm.foodlook
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,49 +17,73 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import es.ucm.foodlook.databinding.ActivityMainBinding
+import java.util.concurrent.ExecutorService
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-private lateinit var binding: ActivityMainBinding
+    val REQUEST_CODE = 100
+    private var check: Boolean = false
+    private lateinit var imageView: ImageView
+    private lateinit var camera: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-     binding = ActivityMainBinding.inflate(layoutInflater)
-     setContentView(binding.root)
+        imageView = findViewById(R.id.imageView)
+        camera = findViewById(R.id.camera)
 
-        setSupportActionBar(binding.toolbar)
+        initPermissions()
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-    }
-override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when(item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        camera.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, REQUEST_CODE)
         }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-    val navController = findNavController(R.id.nav_host_fragment_content_main)
-    return navController.navigateUp(appBarConfiguration)
-            || super.onSupportNavigateUp()
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE && data != null){
+            imageView.setImageBitmap(data.extras!!.get("data") as Bitmap)
+        }
+    }
+
+    // Permissions
+    private fun initPermissions(){
+        if(!getPermission()) setPermission()
+        else check = true
+    }
+    private fun getPermission(): Boolean{
+        return (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+    }
+    private fun setPermission(){
+        val permissionsList = listOf<String>(
+            Manifest.permission.CAMERA
+        )
+        ActivityCompat.requestPermissions(this, permissionsList.toTypedArray(), 1)
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            1 -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Log.i("Permission: ", "Permission has been denied by user")
+                    Toast.makeText(this, "It is not allowed to use the camera.", Toast.LENGTH_SHORT).show()
+                } else {
+                    Log.i("Permission: ", "Permission has been granted by user")
+                    check = true
+                }
+            }
+        }
     }
 }
